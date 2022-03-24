6baef46f4b295674f8413ee440ee20daed24dcd4
Infestas40 <locuras_metallicas@hotmail.com>
2022-03-24 09:09:55 +0100

    subidaarkanoid

--- arkanoid.js
+++ arkanoid.js
@@ -2,7 +2,34 @@
 let bloque=document.getElementById('bloque')
 let cantidad=0;
 
+
+
+
 window.addEventListener('keydown',(e)=>{
-    console.log(e.key)
+    let tecla=e.key
+    
+    switch (tecla){
+        case 'ArrowRight':
+            cantidad+=100
+            bloque.style.transform=`translateX(${cantidad}px)`
+            break;
+            case 'ArrowLeft':
+                cantidad-=100
+                bloque.style.transform=`translateX(${cantidad}px)`
+                break;
+              
+                                                                             
+    }
 
 })
+
+
+
+
+
+
+
+
+
+
+
